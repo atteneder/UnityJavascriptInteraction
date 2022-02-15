695c9a363d0b2394aa20bb5fd8d88a694f97f365
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-02-15 21:37:14 +0100

    fix: Compiler warning due to obsolete API usage

--- Assets/Editor/EditorTools.cs
+++ Assets/Editor/EditorTools.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 
 public class EditorTools {
@@ -14,8 +12,11 @@ public class EditorTools {
 		//PlayerSettings.SetPropertyString("emscriptenArgs", "-Oz -s INLINING_LIMIT=1 -s OUTLINING_LIMIT=0", BuildTargetGroup.WebGL);
 
 		// Get more assert/error information
+#if UNITY_5_6_OR_NEWER
+		PlayerSettings.WebGL.emscriptenArgs = "--profiling -s ASSERTIONS=2";
+#else
 		PlayerSettings.SetPropertyString("emscriptenArgs", "--profiling -s ASSERTIONS=2", BuildTargetGroup.WebGL);
-
+#endif
 		// Integrated kernel sources
 		//PlayerSettings.SetPropertyString("emscriptenArgs", "-Oz -std=c++11 -D NDEBUG", BuildTargetGroup.WebGL);
 	}
@@ -23,6 +24,10 @@ public class EditorTools {
 	[MenuItem("Tools/Unset Emscripten Arguments")]
 	public static void UnsetAdvancedBuildOptionsWebGL() {
 		// Get more assert/error information
+#if UNITY_5_6_OR_NEWER
+		PlayerSettings.WebGL.emscriptenArgs = "";
+#else
 		PlayerSettings.SetPropertyString("emscriptenArgs", "", BuildTargetGroup.WebGL);
+#endif
 	}
 }
